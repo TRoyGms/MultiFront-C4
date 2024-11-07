47d17b31d33ea5b3a1157e3cd16d23812aad5e6e
AlejandroZBarajas <falejandrozb@gmail.com>
2024-11-06 22:32:34 -0600

    main frame declarado

--- game1/src/app/game-module/service/game-logic-service.service.ts
+++ game1/src/app/game-module/service/game-logic-service.service.ts
@@ -1,92 +1,3 @@
-/*
-
-import { Injectable } from '@angular/core';
-import { IWall } from '../Interface/iwall';
-import { IBridge } from '../Interface/ibridge';
-
-@Injectable({
-  providedIn: 'root'
-})
-
-
-
-export class GameLogicServiceService {
-  currentCamera = 1;
-
-  // Definir los puntos de destino del puente
- 
-
-
-  bridge: IBridge ={
-     entry : { x1: 110, x2: 130, y1: 200, y2: 250,color:'black' },
-     exit : { x1: 270, x2: 290, y1: 200, y2: 250,color:'red' }
-  }
-  // Definir un arreglo de muros
-  walls:IWall[] = [
-    { x1: 300, x2: 400, y1: 150, y2: 350,color:'yellow' }, // Primer muro
-    { x1: 450, x2: 550, y1: 100, y2: 300,color:'green' }, // Segundo muro
-    { x1: 600, x2: 700, y1: 200, y2: 400,color:'blue' }, // Tercer muro
-    // Se pueden agregar más muros aquí
-  ];
-
-  secondCamera = {
-    x1: 30,    // Coordenada X del límite izquierdo
-    x2: 80,    // Coordenada X del límite derecho
-    y1: 10,    // Coordenada Y del límite superior
-    y2: 60,    // Coordenada Y del límite inferior
-    zoom: 1,    // Factor de zoom (opcional)
-    angle: 0    // Ángulo de rotación (opcional)
-  };
-  secondCameraComponents = {
-    walls: [
-      { x1: 300, x2: 400, y1: 150, y2: 350, color: 'yellow' },  // Primer muro
-      { x1: 450, x2: 550, y1: 100, y2: 300, color: 'green' },  // Segundo muro
-      { x1: 600, x2: 700, y1: 200, y2: 400, color: 'blue' },   // Tercer muro
-    ],
-    bridge: {
-      entry: { x1: 110, x2: 130, y1: 200, y2: 250, color: 'black' },
-      exit: { x1: 270, x2: 290, y1: 200, y2: 250, color: 'red' },
-    },
-    player: {
-      position: { x: 350, y: 300 }, // Posición inicial del jugador
-      // Otras propiedades del jugador
-    },
-    // Otros componentes que desees añadir
-  };
-
-  
-
-  checkCameraTransition(x: number, y: number) {
-    // Detecta si el jugador está en el área del puente
-    if (x >= this.bridge.entry.x1 && x <= this.bridge.exit.x2 && y >= this.bridge.entry.y1 && y <= this.bridge.entry.y2) {
-      console.log("segunda camara");
-      
-      this.currentCamera = 2;  // Cambia a la segunda cámara
-    } else {
-      console.log('primera camara');
-      
-      this.currentCamera = 1;  // Vuelve a la primera cámara
-    }
-  }
-
-  // Verificar si el jugador está en el área de cualquiera de los muros
-  checkWallCollision(x: number, y: number): boolean {
-    return this.walls.some(wall => (
-      x+20 >= wall.x1 && x < wall.x2 && y+20 >= wall.y1 && y < wall.y2
-    ));
-  }
-
-  // Funciones para verificar si el jugador está en el punto de entrada o salida del puente
-  private isAtBridgeEntry(x: number, y: number): boolean {
-    return (x > this.bridge.entry.x1 && x < this.bridge.entry.x2 && y > this.bridge.entry.y1 && y < this.bridge.entry.y2);
-  }
-
-  private isAtBridgeExit(x: number, y: number): boolean {
-    return (x > this.bridge.exit.x1 && x < this.bridge.exit.x2 && y > this.bridge.exit.y1 && y < this.bridge.exit.y2);
-  }
-}
-*/
-
 import { Injectable } from '@angular/core';
 import { IWall } from '../Interface/iwall';
 import { IBridge } from '../Interface/ibridge';
@@ -105,9 +16,10 @@ export class GameLogicServiceService {
 
   // Definir un arreglo de muros para la primera cámara
   walls: IWall[] = [
-    { x1: 1, x2: 40, y1: 1, y2: 1000, color: 'yellow' }, // Primer muro01
-    { x1: 450, x2: 550, y1: 100, y2: 300, color: 'green' },  // Segundo muro
-    { x1: 600, x2: 700, y1: 200, y2: 400, color: 'blue' },   // Tercer muro
+    { x1: 100, x2: 109, y1: 100, y2: 500, color: 'black' },    // Primer muro verticual
+    { x1: 891, x2: 900, y1: 100, y2: 500, color: 'black' },    // Segundo muro vertical
+    { x1: 100, x2: 900, y1: 100, y2: 109, color: 'black' },  // Primer muro horizontal
+    { x1: 100, x2: 900, y1: 500, y2: 509, color: 'black' },   // Segundo muro Horizontal
   ];
 
   // Definir la segunda cámara
@@ -126,7 +38,7 @@ export class GameLogicServiceService {
       { x1: 30, x2: 40, y1: 10, y2: 30, color: 'black' },  // Primer muro
       { x1: 40, x2: 50, y1: 10, y2: 30, color: 'black' },   // Segundo muro
       { x1: 60, x2: 70, y1: 20, y2: 40, color: 'black' },    // Tercer muro
-      { x1: 60, x2: 70, y1: 20, y2: 40, color: 'black' }
+      { x1: 90, x2: 70, y1: 20, y2: 40, color: 'black' }   //cuarta pared
     ],
     bridge: {
       entry: { x1: 110, x2: 130, y1: 200, y2: 250, color: 'black' },
