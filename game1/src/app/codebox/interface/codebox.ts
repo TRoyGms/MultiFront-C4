d93c49505b7dfa95f32215d87b9b61ed8abc35ec
AlejandroZBarajas <falejandrozb@gmail.com>
2024-11-21 22:18:29 -0600

    servicio carga las codeboxes del nivel

--- game1/src/app/codebox/interface/codebox.ts
+++ game1/src/app/codebox/interface/codebox.ts
@@ -1,2 +1,10 @@
 export interface Codebox {
+    id: string
+    idnivel: number
+    idpuente: number
+    ladox1: number
+    ladox2: number
+    ladoy1: number
+    ladoy2: number
+    textura: string
 }
